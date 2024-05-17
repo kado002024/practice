d14741761a6fb5f3289ea217e6762d85e2f1ad62
kado002024 <sou_0321@yahoo.co.jp>
2024-05-17 17:59:04 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,28 @@
+public abstract class TangibleAsset{
+  private String nama;
+  private int price;
+  private String color;
+
+public TangibleAsset(String name, int price, String color){
+  this.name = name;
+  this.price = price;
+  this.color = color;
+}
+
+public String getName(){
+  return this.name;
+}
+
+public int getPrice(){
+  return this.price
+}
+public String getcColor(){
+  return this.color;
+}
+
+}
+
+  
+
+  
+  
